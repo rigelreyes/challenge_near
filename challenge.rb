70094271cfa8d321e8cf50888a136ec7679cfb4c
Reyes, Rigel <rigel.reyes@oliverwyman.com>
2018-05-13 21:28:00 -0500

    main fix(challenge.rb): Fix missing .description in the do block.

--- challenge.rb
+++ challenge.rb
@@ -15,12 +15,12 @@ time_now = DateTime.now
 
 rsrc.snapshots.each do |snap|
   if "#{snap.description}" =~ /^backup_\d{4}(0?[1-9]|1[012])(0?[1-9]|[12][0-9]|3[01])([01]?\d|2[0-3])([0-5]?\d)([0-5]?\d)\[.{0,}\]$/
-    year = Integer(snap[7..10])
-    month = Integer(snap[11..12])
-    day = Integer(snap[13..14])
-    hour =  Integer(snap[15..16])
-    minutes = Integer(snap[17..18])
-    seconds = Integer(snap[19..20])
+    year = Integer(snap.description[7..10])
+    month = Integer(snap.description[11..12])
+    day = Integer(snap.description[13..14])
+    hour =  Integer(snap.description[15..16])
+    minutes = Integer(snap.description[17..18])
+    seconds = Integer(snap.description[19..20])
     time_snap = DateTime.new(year, month, day, hour, minutes, seconds)
     if time_snap < time_now - 24.hours
       if time_now.day != 15
